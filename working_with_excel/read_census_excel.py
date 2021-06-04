652f26c280afaf71649c027ba953117199a321d1
Gerard Hynes <gkhynes@gmail.com>
2021-06-04 23:05:23 +0100

    Add census excel project

--- working_with_excel/read_census_excel.py
+++ working_with_excel/read_census_excel.py
@@ -1,5 +1,5 @@
 #! python3
-# readCensusExcel.py - Tabulates population and number of census tracts for each county.
+# read_census_excel.py - Tabulates population and number of census tracts for each county.
 
 import openpyxl, pprint
 
@@ -12,9 +12,22 @@ county_data = {}
 
 print('Reading rows...')
 for row in range(2, sheet.max_row + 1):
-    # Each row in the spreadsheet has data fro one census tract
+    # Each row in the spreadsheet has data from one census tract
     state = sheet['B' + str(row)].value
     county = sheet['C' + str(row)].value
     pop = sheet['D' + str(row)].value
+    # Make sure the key for the state exists
+    county_data.setdefault(state, {})
+    # Make sure the key for this county in the state exists
+    county_data[state].setdefault(county, {'tracts':0, 'pop': 0})
+    # Each row represents one census tract so increment by one
+    county_data[state][county]['tracts'] += 1
+    # Increase the county pop by the pop in the census tract
+    county_data[state][county]['pop'] += int(pop)
 
-# TODO: Open a new text file and write the contents of county_data to it
+# Open a new text file and write the contents of county_data to it
+print('Writing results...')
+result_file = open('census2010.py', 'w')
+result_file.write('all_data=' + pprint.pformat(county_data))
+result_file.close()
+print('Done')
