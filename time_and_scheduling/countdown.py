8e9578d9b0f6671cd53c679e60f3d6e5d089f66b
Gerard Hynes <gkhynes@gmail.com>
2021-06-09 23:27:01 +0100

    Add countdown project

---
+++ time_and_scheduling/countdown.py
@@ -0,0 +1,13 @@
+#! python3
+# countdown.py - A simple countdown script
+
+import time, subprocess
+
+time_left = 60
+while time_left > 0:
+    print(time_left, end='\n')
+    time.sleep(1)
+    time_left = time_left - 1
+
+# At the end of the countdown, play a sound file
+subprocess.Popen(['start', 'alarm.wav'], shell=True)
